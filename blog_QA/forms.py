ffcb398a8e0de94faf3452cde2c0159a7144fccb
= <=>
2020-10-20 19:25:22 +0800

    new_topic page updated

--- blog_QA/forms.py
+++ blog_QA/forms.py
@@ -5,8 +5,8 @@ from . models import Topic, Entry
 class TopicForm(forms.ModelForm):
     class Meta:
         model = Topic
-        fields = ['text']
-        labels = {'text': ''}
+        fields = ['text', 'body_text']
+        labels = {'text': '', 'body_text':''}
 
 
 class EntryForm(forms.ModelForm):
